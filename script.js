a602415939d5dd915fc83d65a08f0c20d3383d10
Max Miguta <max_miguta@yahoo.com>
2018-08-30 23:42:08 +0100

    Fixed code validation issues

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ window.onclick = function(event) {
 window.onload = function() {
 	var d = new Date();
 	var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-	month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+	var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 	var n = weekday[d.getDay()] + ", " + d.getDate() + " " + month[d.getMonth()] + " " + d.getFullYear();
 	document.getElementById("date").innerHTML = n;
 };
