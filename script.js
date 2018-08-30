d2e228511019b3ef7bcb9b77d363570789332bc6
Max Miguta <max_miguta@yahoo.com>
2018-08-30 23:28:08 +0100

    Fixed code validation issues

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ modalBtns.forEach(function(btn){
   btn.onclick = function() {
     var modal = btn.getAttribute("data-modal");
     document.getElementById(modal).style.display = "block";
-  }
+  };
 });
 
 var closeBtns = [...document.querySelectorAll(".close")];
@@ -12,19 +12,19 @@ closeBtns.forEach(function(btn){
   btn.onclick = function() {
     var modal = btn.closest(".modal");
     modal.style.display = "none";
-  }
+  };
 });
 
 window.onclick = function(event) {
   if (event.target.className === "modal") {
     event.target.style.display = "none";
   }
-}
+};
 /*Basic date display code*/
 window.onload = function() {
 	var d = new Date();
-	weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-	month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+	weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+	month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 	var n = weekday[d.getDay()] + ", " + d.getDate() + " " + month[d.getMonth()] + " " + d.getFullYear();
 	document.getElementById("date").innerHTML = n;
 };
